8e88e5210e160197bf9d26db88e7a9967751fa6f
fgabrieli@gmail.com <fgabrieli@gmail.com>
2015-01-18 19:26:39 -0300

    Loading jquery file uploader once, when the upload view is active.

--- frontend/modules/upload/Upload.js
+++ frontend/modules/upload/Upload.js
@@ -1,23 +1,62 @@
 /**
- * Upload pictures.
+ * Upload module.
  */
 
 picb.Upload = {
+ fileUploadOpt : {
+  url : '/backend/libs/jquery-file-upload/9.9.2/php/index.php',
+  maxChunkSize : 1 * 1024 * 1024,
+  maxFileSize : (10 * 1024 * 1024 * 1024), // 10mb
+  acceptFileTypes : /(\.|\/)(gif|jpe?g|png)$/i,
+  add : function(e, data) {
+   data.submit().success(function(result, textStatus, jqXHR) {
+    console.log(result);
+   }).error(function(jqXHR, textStatus, errorThrown) {
+
+   }).complete(function(result, textStatus, jqXHR) {
+
+   });
+  },
+  dropZone : {}
+ },
  init : function() {
-  $('#picture').fileupload({
-   url : '/backend/libs/jquery-file-upload/9.9.2/php/index.php',
-   maxChunkSize : 1 * 1024 * 1024,
-   maxFileSize : (10 * 1024 * 1024 * 1024), // 10mb
-   acceptFileTypes : /(\.|\/)(gif|jpe?g|png)$/i,
-   autoUpload : true
-  });
+  // Module initialization
+ },
+ hasFileUpload : function() {
+  var t = picb.Upload;
+
+  var isLoaded = false;
+  try {
+   isLoaded = ($('#picture').fileupload('option', 'url') == t.fileUploadOpt.url);
+  } catch (e) {
+   // leave isLoaded as false
+  }
+
+  return isLoaded;
+ },
+ initFileUpload : function() {
+  var t = picb.Upload;
+  t.fileUploadOpt.dropZone = $('#dropZone');
+  $('#picture').fileupload(t.fileUploadOpt);
  },
  ngController : function($scope) {
   var employeeSrv = picb.Service.Employee;
   $scope.isVisible = (!employeeSrv.hasEmployee());
+
+  $scope.$watch('isVisible', function(isVisible) {
+   var t = picb.Upload;
+
+   if ((typeof isVisible != 'undefined') && (isVisible)) {
+    if (!t.hasFileUpload()) {
+     t.initFileUpload();
+    }
+   }
+  });
  }
 };
 
-$(document).ready(function() {
- picb.Upload.init();
-});
+{
+ $(document).ready(function() {
+  picb.Upload.init();
+ });
+}
